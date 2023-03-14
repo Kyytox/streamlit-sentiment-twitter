f83dbc6d699a5e45a30877c4ca334cafb2bab9e6
Kyytox <thomas.caillault@protonmail.com>
2023-03-14 21:40:26 +0100

    nltk spacy download

--- frequentsWords.py
+++ frequentsWords.py
@@ -8,14 +8,15 @@ from nltk.corpus import stopwords
 import nltk
 
 # DL frequent words FR
-# spacy.cli.download("fr_core_news_sm")
-# nltk.download('stopwords')
+spacy.cli.download("fr_core_news_sm")
+nltk.download('stopwords')
 
 # load model for french language
-nlp = spacy.load("./nltk/fr_core_news_sm/fr_core_news_sm-3.5.0")
+# nlp = spacy.load("./nltk/fr_core_news_sm/fr_core_news_sm-3.5.0")
+nlp = spacy.load("fr_core_news_sm")
 
 # change the path of stopwords and set stopWords for french
-nltk.data.path.append("./nltk")
+# nltk.data.path.append("./nltk")
 stopWords = set(stopwords.words('french'))
 
 
